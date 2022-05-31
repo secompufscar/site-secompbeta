d3bffcb46a2f53dea545f6942f22f17c650cf71d
Joao Pedro Pereira <joaopereira_pedro@hotmail.com>
2022-05-31 13:39:31 -0300

    Testando, acho que agora foi

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -7,7 +7,7 @@ function Sobre() {
             <h3>Sobre</h3><hr size="10" width="100%" />
             <div>
                 <div>
-                  Testando 1 2 3
+                  Testando 1 2 3 4
                 </div>
                 <div className='img-sobre'>
                     <img src={banner} alt="logo" />

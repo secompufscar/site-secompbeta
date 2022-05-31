ec969573f2fcc382d6cb00430211a38e8674d528
Joao Pedro Pereira <joaopereira_pedro@hotmail.com>
2022-05-31 13:23:41 -0300

    Testando, acho que agora foi

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -7,13 +7,7 @@ function Sobre() {
             <h3>Sobre</h3><hr size="10" width="100%" />
             <div>
                 <div>
-                    Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit.
-                    Maxime, nam perspiciatis sint
-                    quas consequuntur sed est magnam
-                    hic ullam reiciendis, nemo ab exercitationem
-                    voluptates provident quasi inventore dolorum
-                    corporis explicabo.
+                  Testando
                 </div>
                 <div className='img-sobre'>
                     <img src={banner} alt="logo" />

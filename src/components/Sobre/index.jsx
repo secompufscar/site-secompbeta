58081f71e6a71091af4407bfe1069cdcfb372b4d
Joao Pedro Pereira <joaopereira_pedro@hotmail.com>
2022-05-31 13:27:50 -0300

    Testando

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -7,7 +7,7 @@ function Sobre() {
             <h3>Sobre</h3><hr size="10" width="100%" />
             <div>
                 <div>
-                  Testando
+                  Testando 1 2 3
                 </div>
                 <div className='img-sobre'>
                     <img src={banner} alt="logo" />

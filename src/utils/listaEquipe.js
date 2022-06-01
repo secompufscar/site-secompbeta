d9cc43b45005f0daff32f69370983be48c871638
João Pedro Pereira <joaopereira_pedro@hotmail.com>
2022-06-01 17:31:32 -0300

    Adicionando redes sociais de alguns membros

--- src/utils/listaEquipe.js
+++ src/utils/listaEquipe.js
@@ -4,7 +4,7 @@ import pietro from 'assets/membros/JF - Pietro Minghini Moralles.png'
 import wesley from 'assets/membros/JF - Wesley Azevedo Souza.png'
 import bruno from 'assets/membros/Patrocínio - Bruno Zenatti de Caires Marcelo.png'
 import gianlucca from 'assets/membros/Sociocult - Gianlucca Pugliesi.jpeg'
-import joao from 'assets/membros/TI - João Pedro Pereira.png'
+import joao from 'assets/membros/TI - João Pedro Pereira.jpg'
 import victor from 'assets/membros/TI - Victor Luis A Antunes.jpeg'
 
 /* Nossa equipe!
@@ -83,17 +83,17 @@ const equipe = [
     {
         nome: 'João Pedro Pereira',
         diretoria: 'TI',
-        linkedin: 'https://linkedin.com',
-        github: 'http://github.com',
-        instagram: 'esf',
+        linkedin: 'http://www.linkedin.com/in/joao-pedropereira',
+        github: 'https://github.com/CaBaNa232',
+        instagram: 'https://www.instagram.com/joaoo.pereira_/',
         img: joao
     },
     {
         nome: 'Victor Luís Aguilar Antunes',
         diretoria: 'TI',
         instagram: 'esf',
-        linkedin: 'https://linkedin.com',
-        github: 'http://github.com',
+        linkedin: 'https://www.linkedin.com/in/victor-lu%C3%ADs-aguilar-antunes-06588221a/',
+        github: 'https://github.com/VLx2000',
         img: victor
     },
 ];
